3b5d48a966424c359bd225ef15eb3c1ab1e21153
Oleg Shlapak <oleg.shlapak.00@gmail.com>
2020-03-20 09:28:13 +0200

    try to pull reqvest

--- index1.js
+++ index1.js
@@ -3,7 +3,6 @@ function randomInt(min, max) {
 	return min + Math.floor((max - min) * Math.random());
 }
 
-alert("111111");
 let woomen =[];
 let tmp;
 let i=0;
@@ -20,7 +19,9 @@ alert("ваша жінка "+ woomen[woomen__number]);
 
 confirm("ви любите сир?")?alert("У вас буде "+randomInt(0, 4)+" дітей"):alert("У вас буде "+randomInt(0, 2)+" дітей");
 
-confirm("Чи любите ви життя?")? alert("США? Та Нє - Китай саме для вас(Удачі) )"):alert("Ну Росія так Росія");
+confirm("Чи любите ви життя?")
+	? alert("США? Та Нє - Китай саме для вас(Удачі) )")
+	: alert("Ну Росія так Росія");
 
 prompt("Ваше улюблене число?");
 let work=["шахтером","Програмістом","безробітнім","поліцейським"];
